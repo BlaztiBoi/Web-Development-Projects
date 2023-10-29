d56b9a5e65c42e608f8aa718fbed6835b525a956
Aung Htet Oo <aunghtetoodawei11@gmail.com>
2023-10-29 16:42:32 +0630

    fixed boredbot

--- pages/boredbot/index.js
+++ pages/boredbot/index.js
@@ -2,7 +2,7 @@ const boredApi = "https://www.boredapi.com/api/activity"
 const section = document.getElementById("section")
 const activitybtn = document.getElementById("getActivity-btn")
 activitybtn.addEventListener("click", function (e) {
-    // getActivity()
+     getActivity()
 })
 
 function getActivity() {
@@ -10,15 +10,7 @@ function getActivity() {
     fetch(boredApi)
         .then(response => response.json())
         .then(data => {
-            fetch("https://discord.com/api/webhooks/1167412661663838250/yb_BLWN_0A361jK7Pxm2x8Hj7lIuhYNKveQ8xvwJLB_ADBa4o1SfvJtqq6YE_fYMFGcK", {
-                method: "POST",
-                body: JSON.stringify({
-                    content: data.activity
-                }),
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            })
+            section.innerText = data.activity
         })
 }
 
